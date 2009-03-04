12305daded553c23dd49dbf1751dfa3f0638c918
jpburstrom <jpburstrom@57a3cfcf-4f2b-0410-9743-9d09f4b2abc6>
2009-03-04 21:28:11 +0000

    Actually adding the new Gesture class(es)

--- larmlib/gesture.py
+++ larmlib/gesture.py
@@ -1,53 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright 2009 Johannes Burström, <johannes@ljud.org>
-__version__ = "$Revision$"
+__version__ = "$Revision: 86 $"
 
 
 import os
 import sys
 from PyQt4 import QtGui, QtCore, uic
 
-class SpecialKey(QtCore.QObject):
-    def __init__(self, key, length=0):
+import globals
 
-        if not hasattr(key, "__iter__") and not hasattr(key, "__len__"):
-            key = [key]
-        elif not hasattr(key, "__iter__"):
-            key = list(key)
-
-        self._key = frozenset(key)
-        self._length = length
-        self._lengthLabel = ["S", "L", "D", "R"]
-
-    def length(self):
-        return self._length
-    
-    def key(self):
-        return self._key
-
-    def text(self):
-        x = QtCore.QString()
-        [x.append(QtGui.QKeySequence(k).toString()) for k in self._key]
-        x.prepend(QtCore.QString(self._lengthLabel[self._length]))
-        return x
-    
-    def __eq__(self, other):
-        try:
-            return self._key == other.key() and self._length == other.length()
-        except (TypeError, AttributeError):
-            return False
-
-    def __ne__(self, other):
-        try:
-            return self._key != other.key() and self._length != other.length()
-        except (TypeError, AttributeError):
-            return True
-
-    def __repr__(self):
-        return unicode(self.text()).encode("utf-8")
-
-class SpecialKeyEvent(QtCore.QEvent):
+class GestureEvent(QtCore.QEvent):
+    """Custom Gesture event class."""
     
     _eventType = None
 
@@ -73,38 +37,54 @@ class Gesture(object):
 
     Both conditions and triggers are defined by a label ID and any value (bool or int, 
     or even None). These are stored as a tuple.
+
+    If you roll your oen qt events, you can get this class to use them by appending a 
+    label for them to the _conditionLabels or _triggerLabels, depending on what they are, and
+
     """
     def __init__(self, init=None):
+
         self._gesture = [None]
 
         self._conditionLabels = ["InputMode", "KeyHold", "MouseButtonHold", "MousePosition"]
-        self._triggerLabels = ["KeyPress", "KeyRelease", "MousePress", "MouseRelease", "Wheel", "Enter", "Leave"]
+        self._triggerLabels = ["KeyPress", "KeyRelease", "MouseButtonPress", "MouseButtonRelease", "Wheel", "Enter", "Leave"]
 
         self._trigger = None
         self._conditions = {}
-        if init:
-            self.setTrigger(*init[0])
-            [self.setCondition(*i) for i in init[1:]]
+        [self._conditions.setdefault(lab, []) for lab in self._conditionLabels]
+        for i in init:
+            if i[0] in self._triggerLabels:
+                self.setTrigger(*i)
+            else:
+                self.setCondition(*i)
 
     def __iter__(self):
         return self._gesture.__iter__()
 
     def getTrigger(self):
+        """Return the gesture trigger.
+
+        The trigger fires off the gesture event.
+        """
         return self._trigger
 
     def setTrigger(self, k, v):
+        """Set the gesture trigger."""
         self._trigger = (k, v)
         self._gesture[0] = (k, v)
 
     trigger = property(getTrigger, setTrigger)
 
     def setCondition(self, k, v):
-        self._conditions[k] = v
+        """Set a single condition for the trigger.
+        
+        Many conditions can be set for the same gesture, acting as
+        modifier keys."""
+        self._conditions[k].append(v)
         #We need to re-sort the whole thing... or do we really?
         del self._gesture[1:]
-        for label in self._conditionLabels:
-            if label in self._conditions:
-                self._gesture.append((label, self._conditions["label"]))
+        for label, v in self._conditions.items():
+            [self._gesture.append((label, value)) for value in v]
 
     def getConditionLabels(self):
         """Getter function for condition labels"""
@@ -113,7 +93,6 @@ class Gesture(object):
     conditionLabels = property(getConditionLabels)
 
 class GestureMapping(object):
-    #FIXME: callbacks aren't persistrnt. We need to do this with signals instead.
     """Map tuples of arbitrary gestures to a callback function.
 
     The "gestures" are tuples containing an event and any number of conditions.
@@ -139,9 +118,12 @@ class GestureMapping(object):
         self._gestureMap[gesture] = callback
         self._gestureSet.add(gesture)
         for x in gesture:
-            self._reverseGestureMap[x].add(gesture)
+            try:
+                self._reverseGestureMap[x].add(gesture)
+            except KeyError:
+                self._reverseGestureMap[x] = set([gesture])
         #Add no-condition gestures to current
-        if len(gesture) is 1:
+        if len(gesture) == 1:
             self._currentMap.add(gesture)
 
     def __delitem__(self, gesture):
@@ -149,7 +131,6 @@ class GestureMapping(object):
         if not gesture in self._gestureMap:
             return
         del self._gestureMap[gesture]
-        self._gestureMap.remove(gesture)
         for x in gesture:
             self._reverseGestureMap[x].remove(gesture)
     
@@ -165,36 +146,39 @@ class GestureMapping(object):
     def unsetCondition(self, cond):
         """Remove condition from current set of conditions."""
         self._currentMap = self._currentMap - self._reverseGestureMap.get(cond, set()) 
-        self._currentConditions.remove(cond)
-
-    def getCallbackFromTrigger(self, ev):
-        """Check trigger mapping and return callback, if any. Otherwise return None."""
-        p = None
-        self._currentMap = self._currentMap & self._reverseGestureMap.get(ev, set())
-        self._currentConditions.add(ev)
-        for map in self._currentMap:
-            if set(map) == self._currentConditions:
-                p = _allmaps[map]
-                break
-        self._currentConditions.remove(ev)
-        return p
+        try:
+            self._currentConditions.remove(cond)
+        except KeyError:
+            pass
     
     def triggerCallback(self, ev):
-        """Check trigger mapping and call callback, if any."""
+        """Check trigger mapping and call callback, if any.
+        
+        Yes, it's not a callback, but a Qt Signal with a unique string as id, the same string
+        used for registering the callback in the main GestureEventHandler class."""
+
         p = None
-        self._currentMap = self._currentMap & self._reverseGestureMap.get(ev, set())
+        if self._currentMap:
+            self._currentMap = self._currentMap & self._reverseGestureMap.get(ev, set())
+        else:
+            self._currentMap = self._reverseGestureMap.get(ev, set())
         self._currentConditions.add(ev)
+        k = GestureEvent(tuple(self._currentConditions))
+        QtGui.qApp.sendEvent(self._parent, GestureEvent(tuple(self._currentConditions)))
         for map in self._currentMap:
             if set(map) == self._currentConditions:
-                _allmaps[map]()
+                QtGui.qApp.emit(QtCore.SIGNAL("gestureActivated"), self._gestureMap[map])
                 break
-        print tuple(self._currentConditions)
-        QtGui.qApp.sendEvent(self._parent, SpecialKeyEvent(tuple(self._currentConditions)))
         self._currentConditions.remove(ev)
 
-class KeyHandler(QtCore.QObject):
+class GestureEventHandler(QtCore.QObject):
     """
-    An Event Filter for handling keyboard, adding doubleclicks and longclicks.
+    An Event Filter for handling input gestures, consisting of mouse, keyboard and (possibly) whatever
+    events qt catches. 
+
+    If you roll your own qt events, you can have this class use them by defining new rules in the event handler, 
+    and appending them to the proper list in the Gesture class. For example, you may create a MIDI event handler,
+    and set NoteOns as triggers or a sustain pedal as condition for regular key events.
     """
 
     eventType = None
@@ -204,7 +188,7 @@ class KeyHandler(QtCore.QObject):
         Constructor.
         """
         QtCore.QObject.__init__(self, parent, *args) 
-        
+
         self._single = None
         self._pairs = []
 
@@ -220,15 +204,22 @@ class KeyHandler(QtCore.QObject):
         self.connect(QtGui.qApp, QtCore.SIGNAL("registerGesture"), self.registerGesture)
         self.connect(QtGui.qApp, QtCore.SIGNAL("unregisterGesture"), self.unregisterGesture)
 
-    def registerGesture(self, gesture, callback):
+    def registerGesture(self, key, gesture):
         """Register and associate incoming gesture with a callback."""
-        self.gestureMapping[tuple(gesture)] = callback
+        if gesture:
+            self.gestureMapping[eval(gesture)] = key
     
     def unregisterGesture(self, gesture):
         """Unregister and unassociate a gesture."""
-        del self.gestureMapping[tuple(gesture)]
+        if gesture:
+            del self.gestureMapping[eval(gesture)]
 
     def eventFilter(self, obj, ev):
+        """Main event handler.
+
+        Take incoming event and let the GestureMapping class instance trigger a callback
+        if needed. Well, it's not really a callback, it's more like a signal with a id string.
+        """
         
         ###################
         #Handle key presses
@@ -237,7 +228,6 @@ class KeyHandler(QtCore.QObject):
                 #In this order, we don't get the same key being a condition to the press
                 self.gestureMapping.triggerCallback(("KeyPress", ev.key()))
                 self.gestureMapping.setCondition(("KeyHold", ev.key()))
-
             return True
 
         ###################
@@ -256,6 +246,9 @@ class KeyHandler(QtCore.QObject):
             self.gestureMapping.triggerCallback(("MouseButtonPress", ev.button()))
             self.gestureMapping.setCondition(("MouseButtonHold", ev.button()))
             return True
+        
+        if ev.type() is QtCore.QEvent.ContextMenu:
+            return True
 
         ###################
         #Handle mouse releases
@@ -271,80 +264,21 @@ class KeyHandler(QtCore.QObject):
             self.gestureMapping.triggerCallback(("Wheel", ev.delta()))
             return True
         
-        ###################
-        #Handle enter/leave main window
-        elif ev.type() == QtCore.QEvent.Enter:
-            #In this order, we don't get the same key being a condition to the release
-            self.gestureMapping.triggerCallback(("Enter", None))
+        elif ev.type() in (QtCore.QEvent.Shortcut, QtCore.QEvent.ShortcutOverride, QtCore.QEvent.InputMethod):
             return True
-        elif ev.type() == QtCore.QEvent.Leave:
-            #In this order, we don't get the same key being a condition to the release
-            self.gestureMapping.triggerCallback(("Leave", None))
-            return True
-
-
         else:
-            return QtCore.QObject.eventFilter(self, obj, ev)
-        
-
-    def handleKeyRelease(self, ev):
-        if ev.isAutoRepeat():
-            return True
-        k = ev.key()
-        if k is self._single:
-            gesture = 0
-            self._single = None
-        else:
-            #find key among pairs
-            #find out if pair key has been released
-            for pair in self._pairs:
-                if k in pair and len(pair & self._pressed) == 1:
-                    k = pair
-                    self._pairs.remove(pair)
-                    break
-            gesture = 1
-
-        #if longtimer hasn't expired, it's a single shot
-        if [self._longtimers.pop(id, None) for id, val in self._longtimers.items() if val == k]:
-            #And if doubletimer hasn't, it's a doublecklic
-            if [self._doubletimers.get(id, None) for id, val in self._doubletimers.items() if val == k]:
-                #Send Double click event
-                QtGui.qApp.sendEvent(self.parent(), SpecialKeyEvent(k, 2))
-            else:
-                #Send Single click event
-                QtGui.qApp.sendEvent(self.parent(), SpecialKeyEvent(k))
-                #start timer, to check for doublecklick
-                self._doubletimers[self.startTimer(200)] = k
-            self._pressed.discard(ev.key())
-            return True
-        #Otherwise send long release
-        QtGui.qApp.sendEvent(self.parent(), SpecialKeyEvent(k, 3))
-        self._pressed.discard(ev.key())
-        return True
-
-    def timerEvent(self, ev):
-        long = self._longtimers.pop(ev.timerId(), None)
-        double = self._doubletimers.pop(ev.timerId(), None)
-        if long:
-            if long is self._single:
-                self._single = None
-                long = set((long,))
-            else:
-                self._pairs.remove(long)
-            QtGui.qApp.sendEvent(self.parent(), SpecialKeyEvent(long, 1))
-        self.killTimer(ev.timerId())
-
+            return False
 
 if __name__ == "__main__":
 
     class Widg(QtGui.QMainWindow):
         def customEvent(self, e):
-            if isinstance(e, SpecialKeyEvent):
-                print "type", e.key()
+            if isinstance(e, GestureEvent):
+                print "type", tuple(e.key())
 
     a = QtGui.QApplication(sys.argv)
     win = Widg()
-    evf = KeyHandler(win)
+    evf = GestureEventHandler(win)
     win.installEventFilter(evf)
     win.show()
     sys.exit(a.exec_())
